df7ecab76760ca913c740d10e67489a04dfd2596
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-11-05 14:37:12 +0300

    Delete checkLoseSnake.js

--- src/helper/checkLoseSnake.js
+++
@@ -1,7 +0,0 @@
-const checkLoseSnake = (snake) => {
-  return snake.body
-    .slice(0, snake.body.length - 1)
-    .find(({ rows, cols }) => rows === snake.rows && cols === snake.cols);
-};
-
-export default checkLoseSnake;
